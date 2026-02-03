b72df9aba28709997cf2ce81f87851f1a0b9be60
mariosmitella <mariosmitella@gmail.com>
2026-02-03 20:09:49 +0000

    Update USB vendor and product IDs

--- firmware/src/tinyusb_stuff.cc
+++ firmware/src/tinyusb_stuff.cc
@@ -34,8 +34,8 @@
 
 // These IDs are bogus. If you want to distribute any hardware using this,
 // you will have to get real ones.
-#define USB_VID 0xCAFE
-#define USB_PID 0xBAF2
+#define USB_VID 0x0AE4
+#define USB_PID 0x0702
 
 tusb_desc_device_t desc_device = {
     .bLength = sizeof(tusb_desc_device_t),
