caee9916b54191209ede9a2e4bf9c2b3edc7c7d4
mariosmitella <mariosmitella@gmail.com>
2026-02-08 10:59:31 +0000

    Update report descriptor for HoriPs4 TO Egret

--- firmware/src/our_descriptor.cc
+++ firmware/src/our_descriptor.cc
@@ -269,43 +269,28 @@ const uint8_t our_report_descriptor_absolute[] = {
 
 const uint8_t our_report_descriptor_horipad[] = {
     0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
-    0x09, 0x05,        // Usage (Game Pad)
+    0x09, 0x04,        // Usage (Joystick)
     0xA1, 0x01,        // Collection (Application)
     0x15, 0x00,        //   Logical Minimum (0)
     0x25, 0x01,        //   Logical Maximum (1)
     0x35, 0x00,        //   Physical Minimum (0)
     0x45, 0x01,        //   Physical Maximum (1)
     0x75, 0x01,        //   Report Size (1)
-    0x95, 0x0E,        //   Report Count (14)
-    0x05, 0x09,        //   Usage Page (Button)
+    0x95, 0x0C,        //   Report Count (12)
+    0x05, 0x09,        //   Usage Page (Button)F
     0x19, 0x01,        //   Usage Minimum (0x01)
-    0x29, 0x0E,        //   Usage Maximum (0x0E)
+    0x29, 0x0C,        //   Usage Maximum (0x0C)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x95, 0x02,        //   Report Count (2)
+    0x95, 0x04,        //   Report Count (4)
     0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
-    0x25, 0x07,        //   Logical Maximum (7)
-    0x46, 0x3B, 0x01,  //   Physical Maximum (315)
-    0x75, 0x04,        //   Report Size (4)
-    0x95, 0x01,        //   Report Count (1)
-    0x65, 0x14,        //   Unit (System: English Rotation, Length: Centimeter)
-    0x09, 0x39,        //   Usage (Hat switch)
-    0x81, 0x42,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,Null State)
-    0x65, 0x00,        //   Unit (None)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x26, 0xFF, 0x00,  //   Logical Maximum (255)
     0x46, 0xFF, 0x00,  //   Physical Maximum (255)
     0x09, 0x30,        //   Usage (X)
     0x09, 0x31,        //   Usage (Y)
-    0x09, 0x32,        //   Usage (Z)
-    0x09, 0x35,        //   Usage (Rz)
     0x75, 0x08,        //   Report Size (8)
-    0x95, 0x04,        //   Report Count (4)
+    0x95, 0x02,        //   Report Count (2)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x75, 0x08,        //   Report Size (8)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0xC0,              // End Collection
 };
 
@@ -588,7 +573,7 @@ bool kb_mouse_should_cause_wakeup(uint8_t report_id, const uint8_t* buffer, uint
     return false;
 }
 
-static const uint8_t horipad_neutral[] = { 0x00, 0x00, 0x0F, 0x80, 0x80, 0x80, 0x80, 0x00 };
+static const uint8_t horipad_neutral[] = { 0x00, 0x00, 0x80, 0x80 };
 
 static const uint8_t egret_neutral[] = { 0x00, 0x00, 0x80, 0x80 };
 
@@ -701,8 +686,8 @@ const our_descriptor_def_t our_descriptors[] = {
         .idx = 2,
         .descriptor = our_report_descriptor_horipad,
         .descriptor_length = sizeof(our_report_descriptor_horipad),
-        .vid = 0x0F0D,
-        .pid = 0x00C1,
+        .vid = 0x0AE4,
+        .pid = 0x0702,
         .handle_received_report = do_handle_received_report,
         .clear_report = horipad_clear_report,
         .default_value = horipad_default_value,
